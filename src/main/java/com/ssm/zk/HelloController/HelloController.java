ca8fb4a2bb0c968a4f03fece91c3f5d9ab5f2e6c
zhoukun <zhoukun@utry.cn>
2019-03-26 17:32:30 +0800

    测试springboot一些基本操作

---
+++ src/main/java/com/ssm/zk/HelloController/HelloController.java
@@ -0,0 +1,21 @@
+package com.ssm.zk.HelloController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Classname HelloController
+ * @Description TODO
+ * @Date 2019-03-26 17:20
+ * @Author zhoukun
+ */
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String index(){
+        return "Hello World";
+    }
+
+}
--- src/test/java/com/ssm/zk/ZkApplicationTests.java
+++ src/test/java/com/ssm/zk/ZkApplicationTests.java
@@ -1,16 +1,39 @@
 package com.ssm.zk;
 
+import com.ssm.zk.HelloController.HelloController;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
+@WebAppConfiguration
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = MockServletContext.class)
 public class ZkApplicationTests {
 
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception{
+        mockMvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+    }
+
     @Test
-    public void contextLoads() {
+    public void getHello() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello World")));
     }
 
 }
