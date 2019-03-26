a3c4ea9dcda55b734ad23b9ee529f699ba68d006
zhoukun <zhoukun@utry.cn>
2019-03-26 17:10:18 +0800

    初步搭建环境

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # springboot
+涉及springboot的搭建与项目的运行。
--- src/main/java/com/ssm/zk/ZkApplication.java
+++ src/main/java/com/ssm/zk/ZkApplication.java
@@ -2,12 +2,21 @@ package com.ssm.zk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 @SpringBootApplication
 public class ZkApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZkApplication.class, args);
+    }
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello1";
     }
 
 }
