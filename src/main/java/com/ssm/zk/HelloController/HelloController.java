76fb8e945f496011e50614e0b5d583a51808c50a
zhoukun <zhoukun@utry.cn>
2019-03-27 16:37:08 +0800

    去掉多余的代码

--- src/main/java/com/ssm/zk/HelloController/HelloController.java
+++ src/main/java/com/ssm/zk/HelloController/HelloController.java
@@ -1,5 +1,6 @@
 package com.ssm.zk.HelloController;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,11 @@ public class HelloController {
     @RequestMapping("/hello")
     public String index(){
         return "Hello World";
+    }
+
+    @PostMapping("/hhh")
+    public String hhh(){
+        return "jjj";
     }
 
 }
